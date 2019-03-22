3c398f21b1950328dcdeceff39c542a5ee134abc
Benten559 <Benten559@my.fresnostate.edu>
2019-03-22 07:49:45 -0700

    removed a func and added comments

--- avltree.h
+++ avltree.h
@@ -11,7 +11,6 @@ template <class type> class avltree{
             avltree();
             void traverse(tnode<type>* root);
             void display();
-            bool isEmpty();
             void display(tnode<type>* root);
             void insert(type item);//helper
             tnode<type>* insertion(tnode<type>* tree, type item);
@@ -29,7 +28,7 @@ avltree<type>::avltree(){
     root = NULL;
 }
 template<class type>
-void avltree<type>::traverse(tnode<type>* root){
+void avltree<type>::traverse(tnode<type>* root){ //inorder utility
     if (root == NULL) return;
     else {
         traverse(root->getLeft());
@@ -38,23 +37,19 @@ void avltree<type>::traverse(tnode<type>* root){
     }
 }
 template<class type> 
-void avltree<type>::display(){
+void avltree<type>::display(){ //primary for cout
     if (root == NULL) return;
     else {
         traverse(root);
     }
 }
 template<class type>
-bool avltree<type>::isEmpty(){
-    return (root == NULL);
-}
-template<class type>
-void avltree<type>::insert(type item){
+void avltree<type>::insert(type item){ //primary insertion function
     root = insertion(root,item);
 }
 template<class type>
-tnode<type>* avltree<type>::insertion(tnode<type>* tree, type item){
-    if (tree == NULL){
+tnode<type>* avltree<type>::insertion(tnode<type>* tree, type item){ //utility function for inserting
+    if (tree == NULL){  //tree empty so it becomes the root
       tnode<type>* temp = new tnode<type>(item,NULL,NULL);
       tree = temp;
     }
@@ -70,7 +65,7 @@ tnode<type>* avltree<type>::insertion(tnode<type>* tree, type item){
     return tree;
 }
 template<class type>
-tnode<type>* avltree<type>::getBottom(tnode<type>* tree){
+tnode<type>* avltree<type>::getBottom(tnode<type>* tree){ //returns the smallest node, used to find placeholder for an entire left subtree
     tnode<type>* temp = tree;
     while (temp->getLeft() != NULL){
         temp = temp->getLeft();
@@ -81,7 +76,7 @@ template <class type>
 tnode<type>* avltree<type>::delNode(tnode<type>* tree, type item){
     if( tree == NULL) return NULL;  //EMPTY TREE
     else if (tree->getItem() == item){  //FOUND NODE TO BE DELETED
-        if ((tree->getRight() == NULL) && (tree->getLeft() != NULL)){ //CASE 1
+        if ((tree->getRight() == NULL) && (tree->getLeft() != NULL)){ //CASE 1  FOUND AND HAS A LEFT SUBTREE
             tnode<type>* temp = tree->getLeft();
             tree->setLeft(NULL);
             tree->setRight(NULL);
@@ -89,7 +84,7 @@ tnode<type>* avltree<type>::delNode(tnode<type>* tree, type item){
             temp->setHeight(heightUpdate(temp));
             return temp;
         }
-        else if ((tree->getLeft() == NULL) && (tree->getRight() != NULL)){//CASE 2
+        else if ((tree->getLeft() == NULL) && (tree->getRight() != NULL)){//CASE 2: FOUND AND HAS A RIGHT SUBTREE
             tnode<type>* temp = tree->getRight();//this will be new root
             tree->setRight(NULL);
             tree->setLeft(NULL);
@@ -100,7 +95,7 @@ tnode<type>* avltree<type>::delNode(tnode<type>* tree, type item){
         else{                                   //CASE 3: FOUND THE NODE AND IT HAS A LEFT AND RIGHT SUBTREE
             tnode<type>* temp = tree->getRight();//this will be new root
             tnode<type>* bottom = getBottom(temp);// this left subtree goes to the bottom of the smallest right subtree node
-            bottom->setLeft(tree->getLeft());
+            bottom->setLeft(tree->getLeft());   
             tree->setRight(NULL);
             tree->setLeft(NULL);
             delete tree;
@@ -108,7 +103,7 @@ tnode<type>* avltree<type>::delNode(tnode<type>* tree, type item){
             return temp;
         }
     }
-    else if(tree->getItem() > item){
+    else if(tree->getItem() > item){ //item wasnt found so keep searching through tree
         tree->setLeft(delNode(tree->getLeft(), item));
     }
     else {
@@ -117,17 +112,17 @@ tnode<type>* avltree<type>::delNode(tnode<type>* tree, type item){
 }
 
 template <class type>
-void avltree<type>::deleteNode(type item){
+void avltree<type>::deleteNode(type item){ //primary function for deletion
     root = delNode(root, item);
 }
 
 template <class type>
-int avltree<type>::max(int a, int b){
+int avltree<type>::max(int a, int b){ //needed for node height function
     if (a > b) return a;
     else return b;
 }
 template <class type>
-int avltree<type>::heightUpdate(tnode<type>* tree){
+int avltree<type>::heightUpdate(tnode<type>* tree){//returns the height of each node after inserting and deleting
     if (tree == NULL)return -1;
     else{
        return max(heightUpdate(tree->getLeft()),heightUpdate(tree->getRight())) + 1;
